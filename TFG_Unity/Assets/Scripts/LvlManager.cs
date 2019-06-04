1d90dd999d1577f37e95a47150bb1ac117be0580
Guillem <gimell3000@hotmail.com>
2019-06-04 22:56:26 +0200

    Change color level selected

--- TFG_Unity/Assets/Scripts/LvlManager.cs
+++ TFG_Unity/Assets/Scripts/LvlManager.cs
@@ -9,15 +9,19 @@ public class LvlManager : MonoBehaviour {
 	public Text score;
 	int[] bs;
 	int lvl;
+	GameObject[] btnlvl;
 
 	void Start(){
 		lvl = HomeInteractiveScript.lvl;
 		bs = HomeInteractiveScript.bs;
 		score.text = bs [lvl].ToString();
+		btnlvl = GameObject.FindGameObjectsWithTag("BtnLvl");	
 	}
 
 	public void loadID (int i) {
 		lvl = i;
+		for(int j = 0; j < btnlvl.Length; j++)
+			btnlvl [j].GetComponent<Image> ().color = j.Equals(i) ? Color.gray : Color.white;
 		score.text = bs [i].ToString ();
 		//Application.LoadLevel (i.ToString ());
 	}
